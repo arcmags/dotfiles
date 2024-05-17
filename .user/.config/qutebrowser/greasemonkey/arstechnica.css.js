bffa9a2626b475938d43fa4f3b85907ddf3cac12
arcmags <c.magyar.ec@gmail.com>
2024-05-16 23:52:28 -0500

    repo-update 2024-05-16 23:52

---
+++ .user/.config/qutebrowser/greasemonkey/arstechnica.css.js
@@ -0,0 +1,24 @@
+// ==UserScript==
+// @name        Ars Technica
+// @include     *://arstechnica.com/*
+// ==/UserScript==
+
+/* ~/.config/qutebrowser/greasemonkey/arstechnica.css.js :: */
+/* intended for qutebrowser with darkmode enabled */
+
+GM_addStyle(`
+    .ad.ad.ad,
+    .ad_wrapper {
+        display: none !important;
+    }
+
+    .listing-top.with-feature .article.top-feature figure .listing,
+    .listing-top.with-feature .article.top-latest figure .listing,
+    .listing-top.with-feature .article figure .listing,
+    .listing-earlier .article figure .listing,
+    .listing-latest .article figure .listing,
+    .listing-rest .article figure .listing,
+    .with-xrail .xrail .featured-video .video-thumbnail {
+        background: none;
+    }
+`)
